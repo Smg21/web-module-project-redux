1ccdd5a55c509213acfa3b569fe260f4a1a0c34b
Sabrina <Smg21022@gmail.com>
2023-08-03 13:07:27 -0700

    setup redux

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,50 +1,88 @@
 import React from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import MovieListItem from './MovieListItem';
+import MovieFooter from './MovieFooter';
 
-const Movie = (props) => {
-    const { id } = useParams();
-    const { push } = useHistory();
+const MovieList = ({ movies }) => {
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-    const movies = [];
-    const movie = movies.find(movie=>movie.id===Number(id));
+        <tbody>
+          {movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
+
+      <MovieFooter totalMovies={movies.length} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  movies: state.movies,
+});
+
+export default connect(mapStateToProps)(MovieList);
+
+
+// import React from 'react';
+// import { useParams, useHistory } from 'react-router-dom';
+
+// const Movie = (props) => {
+//     const { id } = useParams();
+//     const { push } = useHistory();
+
+//     const movies = [];
+//     const movie = movies.find(movie=>movie.id===Number(id));
     
-    return(<div className="modal-page col">
-        <div className="modal-dialog">
-            <div className="modal-content">
-                <div className="modal-header">						
-                    <h4 className="modal-title">{movie.title} Details</h4>
-                </div>
-                <div className="modal-body">
-                    <div className="flexContainer">
+//     return(<div className="modal-page col">
+//         <div className="modal-dialog">
+//             <div className="modal-content">
+//                 <div className="modal-header">						
+//                     <h4 className="modal-title">{movie.title} Details</h4>
+//                 </div>
+//                 <div className="modal-body">
+//                     <div className="flexContainer">
 
-                        <section className="movie-details">
-                            <div>
-                                <label>Title: <strong>{movie.title}</strong></label>
-                            </div>
-                            <div>
-                                <label>Director: <strong>{movie.director}</strong></label>
-                            </div>
-                            <div>
-                                <label>Genre: <strong>{movie.genre}</strong></label>
-                            </div>
-                            <div>
-                                <label>Metascore: <strong>{movie.metascore}</strong></label>
-                            </div>
-                            <div>
-                                <label>Description:</label>
-                                <p><strong>{movie.description}</strong></p>
-                            </div>
-                        </section>
+//                         <section className="movie-details">
+//                             <div>
+//                                 <label>Title: <strong>{movie.title}</strong></label>
+//                             </div>
+//                             <div>
+//                                 <label>Director: <strong>{movie.director}</strong></label>
+//                             </div>
+//                             <div>
+//                                 <label>Genre: <strong>{movie.genre}</strong></label>
+//                             </div>
+//                             <div>
+//                                 <label>Metascore: <strong>{movie.metascore}</strong></label>
+//                             </div>
+//                             <div>
+//                                 <label>Description:</label>
+//                                 <p><strong>{movie.description}</strong></p>
+//                             </div>
+//                         </section>
                         
-                        <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
-                        </section>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>);
-}
+//                         <section>
+//                             <span className="m-2 btn btn-dark">Favorite</span>
+//                             <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+//                         </section>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>);
+// }
 
-export default Movie;
+// export default Movie;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,84 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
-const MovieList = (props)=> {
-    const movies = [];
+const MovieList = ({ movies }) => {
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
+      <MovieFooter totalMovies={movies.length} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  movies: state.movies,
+});
+
+export default connect(mapStateToProps)(MovieList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import MovieListItem from './MovieListItem';
+// import MovieFooter from './MovieFooter';
+
+// const MovieList = (props)=> {
+//     const movies = [];
+
+//     return (
+//         <div className="col">
+//             <table className="table table-striped table-hover">
+//                 <thead>
+//                 <tr>
+//                     <th>Title</th>
+//                     <th>Director</th>
+//                     <th>Genre</th>
+//                     <th>Metascore</th>
+//                     <th></th>
+//                 </tr>
+//                 </thead>
+
+//                 <tbody>
+//                     {
+//                         movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+//                     }
+//                 </tbody>
+//             </table>
             
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+//             <MovieFooter totalMovies={movies.length}/>
+//         </div>
+//     );
+// }
 
-export default MovieList;
+// export default MovieList;
