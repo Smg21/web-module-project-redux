641286489e4f2f5d50c0b2734ad858aeba7fcc39
Sabrina <Smg21022@gmail.com>
2023-08-03 18:56:00 -0700

    kinda got working

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch, Redirect } from 'react-router-dom';
@@ -7,8 +9,9 @@ import Movie from './Movie';
 import MovieHeader from './MovieHeader';
 import AddMovieForm from './AddMovieForm';
 import FavoriteMovieList from './FavoriteMovieList';
+import { toggleFavorites } from '../actions/favoritesActions';
 
-const App = ({ displayFavorites }) => {
+const App = ({ displayFavorites, toggleFavorites }) => {
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
@@ -18,8 +21,11 @@ const App = ({ displayFavorites }) => {
       <div className="container">
         <MovieHeader />
         <div className="row">
-          {displayFavorites && <FavoriteMovieList />}
-        
+          <div className="col-sm-6 headerBar">
+            <div className="btn btn-sm btn-primary" onClick={toggleFavorites}>
+              <span>{displayFavorites ? 'Hide' : 'Show'} Favorites</span>
+            </div>
+          </div>
           <Switch>
             <Route exact path="/movies/add">
               <AddMovieForm />
@@ -39,7 +45,58 @@ const App = ({ displayFavorites }) => {
 };
 
 const mapStateToProps = (state) => ({
-  displayFavorites: state.favorites.displayFavorites, // Access the 'displayFavorites' state from the 'favorites' state
+  displayFavorites: state.favorites.displayFavorites,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = {
+  toggleFavorites: toggleFavorites,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { Route, Switch, Redirect } from 'react-router-dom';
+
+// import MovieList from './MovieList';
+// import Movie from './Movie';
+// import MovieHeader from './MovieHeader';
+// import AddMovieForm from './AddMovieForm';
+// import FavoriteMovieList from './FavoriteMovieList';
+
+// const App = ({ displayFavorites }) => {
+//   return (
+//     <div>
+//       <nav className="navbar navbar-dark bg-dark">
+//         <span className="navbar-brand">Redux Module Project</span>
+//       </nav>
+
+//       <div className="container">
+//         <MovieHeader />
+//         <div className="row">
+//           {displayFavorites && <FavoriteMovieList />}
+        
+//           <Switch>
+//             <Route exact path="/movies/add">
+//               <AddMovieForm />
+//             </Route>
+//             <Route exact path="/movies/:id" component={Movie} />
+//             <Route path="/movies">
+//               <MovieList />
+//             </Route>
+//             <Route path="/">
+//               <Redirect to="/movies" />
+//             </Route>
+//           </Switch>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = (state) => ({
+//   displayFavorites: state.favorites.displayFavorites, // Access the 'displayFavorites' state from the 'favorites' state
+// });
+
+// export default connect(mapStateToProps)(App);
--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,10 +1,10 @@
-// MovieHeader.js
 
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { toggleFavorites } from '../actions/favoritesActions';
 
-const MovieHeader = ({ appTitle, displayFavorites }) => {
+const MovieHeader = ({ appTitle, displayFavorites, toggleFavorites }) => {
   return (
     <div className="table-title">
       <div className="row">
@@ -12,7 +12,7 @@ const MovieHeader = ({ appTitle, displayFavorites }) => {
           <h2>{appTitle}</h2>
         </div>
         <div className="col-sm-6 headerBar">
-          <div className="btn btn-sm btn-primary">
+          <div className="btn btn-sm btn-primary" onClick={toggleFavorites}>
             <span>{displayFavorites ? 'Hide' : 'Show'} Favorites</span>
           </div>
           <Link to="/movies" className="btn btn-sm btn-primary">
@@ -29,11 +29,52 @@ const MovieHeader = ({ appTitle, displayFavorites }) => {
 };
 
 const mapStateToProps = (state) => ({
-  appTitle: state.movies.appTitle, // Access the 'appTitle' state from the 'movies' state
-  displayFavorites: state.favorites.displayFavorites, // Access the 'displayFavorites' state from the 'favorites' state
+  appTitle: state.movies.appTitle,
+  displayFavorites: state.favorites.displayFavorites,
 });
 
-export default connect(mapStateToProps)(MovieHeader);
+const mapDispatchToProps = {
+  toggleFavorites: toggleFavorites,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieHeader);
+
+// // MovieHeader.js
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+
+// const MovieHeader = ({ appTitle, displayFavorites }) => {
+//   return (
+//     <div className="table-title">
+//       <div className="row">
+//         <div className="col-sm-6">
+//           <h2>{appTitle}</h2>
+//         </div>
+//         <div className="col-sm-6 headerBar">
+//           <div className="btn btn-sm btn-primary">
+//             <span>{displayFavorites ? 'Hide' : 'Show'} Favorites</span>
+//           </div>
+//           <Link to="/movies" className="btn btn-sm btn-primary">
+//             View All Movies
+//           </Link>
+//           <Link to="/movies/add" className="btn btn-sm btn-success">
+//             <i className="material-icons">&#xE147;</i>{' '}
+//             <span>Add New Movie</span>
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = (state) => ({
+//   appTitle: state.movies.appTitle, // Access the 'appTitle' state from the 'movies' state
+//   displayFavorites: state.favorites.displayFavorites, // Access the 'displayFavorites' state from the 'favorites' state
+// });
+
+// export default connect(mapStateToProps)(MovieHeader);
 
 
 //hh
